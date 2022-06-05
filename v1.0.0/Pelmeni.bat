a802563a519311f35d2bc6eb09a195f97885d202
Vovo4k <93855040+Vovo4k@users.noreply.github.com>
2022-06-05 16:14:49 +0300

    Update Pelmeni.bat

--- v1.0.0/Pelmeni.bat
+++ v1.0.0/Pelmeni.bat
@@ -1,3 +1,3 @@
 @echo off
-for /l %%a in (1,1,4) do for /l %%b in (1,1,4) do SYSTEM32\Insertbmp /IMG:"g\Load%%b.bmp" /x:440 /y:400 /z:100
+for /l %%a in (1,1,4) do for /l %%b in (1,1,4) do SYSTEM32\Insertbmp /IMG:"IMG\Load%%b.bmp" /x:440 /y:400 /z:100
 pause 
